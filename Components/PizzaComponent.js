7a6e06dc9ed21c3abb55cdc067514257528c8086
AmanWorku <amexworku2014@gmail.com>
2023-03-01 19:53:40 +0300

    Add a button and style it

--- Components/PizzaComponent.js
+++ Components/PizzaComponent.js
@@ -1,26 +1,53 @@
-import { Pressable, StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import ModalDropdown from "react-native-modal-dropdown";
 
-const PizzaComponent = ({pizza}) => {
-    const data = [pizza];
-  return (
-    <View>
-        {data.map((item, index) => (
-            <Pressable>
-                <Image style={{height: 200, aspectRatio:5/5, resizeMode: 'cover'}}source={{uri:item.image}} />
-                <View style={{backgroundColor: "#006491", padding:10}}>
-                    <Text style={{fontSize: 17, fontWeight: "bold", color: "white"}}>{item.name.substr(0,15) + "..."}</Text>
-                    <Text style={{color:"pink"}}>{item.description.substr(0,25) + "..."}</Text>
-                </View>
-                <Pressable>
-                    
-                </Pressable>
-            </Pressable>
-        ))}
-    </View>
-  )
-}
+const PizzaComponent = ({ pizza }) => {
+	const options = ["Regular", "Medium", "Large"];
+	const data = [pizza];
+	return (
+		<View>
+			{data.map((item, index) => (
+				<Pressable>
+					<Image
+						style={{ height: 200, aspectRatio: 5 / 5, resizeMode: "cover" }}
+						source={{ uri: item.image }}
+					/>
+					<View style={{ backgroundColor: "#006491", padding: 10 }}>
+						<Text style={{ fontSize: 17, fontWeight: "bold", color: "white" }}>
+							{item.name.substr(0, 15) + "..."}
+						</Text>
+						<Text style={{ color: "pink", marginTop: 4 }}>
+							{item.description.substr(0, 25) + "..."}
+						</Text>
+						<Pressable
+							style={{
+								marginTop: 5,
+								flexDirection: "row",
+								alignItems: "center",
+								marginTop: 10,
+							}}>
+							<ModalDropdown
+								dropdownStyle={{ width: 60, height: 100 }}
+								style={{ width: 60 }}
+								options={options}></ModalDropdown>
+							<Pressable
+								style={{
+									backgroundColor: "#03C03C",
+									padding: 5,
+									marginLeft: 15,
+									borderRadius: 4,
+								}}>
+								<Text>Add To Cart</Text>
+							</Pressable>
+						</Pressable>
+					</View>
+				</Pressable>
+			))}
+		</View>
+	);
+};
 
-export default PizzaComponent
+export default PizzaComponent;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
